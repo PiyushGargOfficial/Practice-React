b802836629619f86c0b5856d55037609d09bbe0b
Piyush <piyushgargofficial@gmail.com>
2021-04-17 21:20:15 +0530

    Final Commit Day9 2

--- reactpractice/src/App.jsx
+++ reactpractice/src/App.jsx
@@ -2,16 +2,27 @@ import React from 'react';
 // import Practice from './NewPractice/NewPractice'
 import Navbar from './NewPractice/Navbar'
 import Home from './NewPractice/Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Create from './NewPractice/Create';
+import BlogDetails from './NewPractice/BlogDetails'
 
 const App = () => {
     return ( 
         <>
+        <Router>
           <div className="App">
           <Navbar />
           <div className="content">
-            <Home />
+          <Switch>
+            <Route path='/' component={Home} exact/>
+            <Route path='/create' component={Create} exact/>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+          </Switch>
           </div>
           </div>
+        </Router>
         </>
      );
 }
--- reactpractice/src/NewPractice/BlogList.jsx
+++ reactpractice/src/NewPractice/BlogList.jsx
@@ -1,21 +1,40 @@
-const BlogList = ({blogData, title}) => {
+// const BlogList = ({blogData, title}) => {
 
-    // const blogs = props.blogData;
-    // const title = props.title;
-    // console.log(props)
+//     // const blogs = props.blogData;
+//     // const title = props.title;
+//     // console.log(props)
 
-    return ( 
-        <div className='blog-list'>
-        <h2>{title}</h2>
-         { blogData.map((blog) =>(
-                <div className='blog-preview' key={blog.id}>
-                <h2>{ blog.title }</h2>
-                <p>Written by { blog.author }</p>
-                </div>
-           ))
-           }
+//     return ( 
+//         <div className='blog-list'>
+//         <h2>{title}</h2>
+//          { blogData.map((blog) =>(
+//                 <div className='blog-preview' key={blog.id}>
+//                 <h2>{ blog.title }</h2>
+//                 <p>Written by { blog.author }</p>
+//                 </div>
+//            ))
+//            }
+//         </div>
+//      );
+// }
+ 
+// export default BlogList;
+
+import { Link } from 'react-router-dom';
+
+const BlogList = ({ blogData }) => {
+  return (
+    <div className="blog-list">
+      {blogData.map(blog => (
+        <div className="blog-preview" key={blog.id} >
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{ blog.title }</h2>
+            <p>Written by { blog.author }</p>
+          </Link>
         </div>
-     );
+      ))}
+    </div>
+  );
 }
  
 export default BlogList;
